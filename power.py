e0b17e3c1b03cdb7f6b2df2e0a6a0d9e45564363
ghosharatrika <150947458+ghosharatrika@users.noreply.github.com>
2023-11-16 00:19:25 +0530

    Update power.py
    Using for-else construct and list index

--- power.py
+++ power.py
@@ -1,11 +1,9 @@
 L = [1, 2, 4, 8, 16, 32, 64]
 X = 5
 found = False
-i = 0
-while i < len(L):
-    if 2 ** X == L[i]:
-        print('at index', i)
+for index, val in enumerate(L):
+    if 2 ** X == val:
+        print('at index', index)
         break
-    i = i+1
 else:
     print(X, 'not found')
