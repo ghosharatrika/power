b6026f44c22f2dabd266d0269eb02ae5590430ac
ghosharatrika <150947458+ghosharatrika@users.noreply.github.com>
2023-11-16 17:10:41 +0530

    Delete fibonacci_no.py

--- fibonacci_no.py
+++
@@ -1,7 +0,0 @@
-fibonacci_numbers = [0, 1]
-
-while len(fibonacci_numbers) < 10:
-    next_number = fibonacci_numbers[-1] + fibonacci_numbers[-2]
-    fibonacci_numbers.append(next_number)
-
-print(fibonacci_numbers)
